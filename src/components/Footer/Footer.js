5a4beef1aaba49af98e8ce385498628e68fea2cd
WRFMIXO <jakim2621@gmail.com>
2024-07-11 16:14:22 +0100

    uupdates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,14 +15,14 @@ const Footer = () => (
     <footer className='footer-section pt-4'>
     <div className="container">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-xl-4 ld-4 md-4 sm-12">
           <h5>Entreprise</h5>
           <img src={logo} width="100px" alt="Logo" className="mb-2" />
           <p>GT TECH & Co</p>
           <p>+237 698 60 31 78</p>
           <p>jakim2621@gmail.com</p>
         </div>
-        <div className="col-md-4">
+        <div className="col-xl-4 ld-4 md-4 sm-12">
           <h5>Liens</h5>
           <ul className="list-unstyled">
             <li><Link to="/gttechandco">Accueil</Link></li>
@@ -31,7 +31,7 @@ const Footer = () => (
             <li><Link to="/gttechandco/contact">Contacts</Link></li>
           </ul>
         </div>
-        <div className="col-md-4">
+        <div className="col-xl-4 ld-4 md-4 sm-12">
           <h5>Suivez-nous</h5>
           <ul className="list-unstyled">
             <li><a href="https://facebook.com/profile.php?id=61553743893716">Facebook</a></li>
